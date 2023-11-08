2b0626b5134cffcdad88343eabdd04fd05e4b58d
Ethan Gray <etan328@gmail.com>
2023-11-08 08:32:51 -0800

    Submodule work pt. 2

---
+++ bin/camera.d
@@ -0,0 +1,92 @@
+bin/camera.o: src/camera.cpp src/camera.hpp src/player.hpp \
+ include/raylib-cpp.hpp include/./AudioDevice.hpp include/././raylib.hpp \
+ include/././raylib.h include/././raylib-cpp-utils.hpp \
+ include/././RaylibException.hpp include/./././raylib.hpp \
+ include/./AudioStream.hpp include/./BoundingBox.hpp \
+ include/./Camera2D.hpp include/././Vector2.hpp include/./././raymath.hpp \
+ include/./././raymath.h include/./././raylib-cpp-utils.hpp \
+ include/./Camera3D.hpp include/././Vector3.hpp include/./Color.hpp \
+ include/././Vector4.hpp include/./Font.hpp \
+ include/././TextureUnmanaged.hpp include/./././Vector2.hpp \
+ include/./././Material.hpp include/././././raylib.hpp \
+ include/././././raylib-cpp-utils.hpp include/./././RaylibException.hpp \
+ include/./././Image.hpp include/././././RaylibException.hpp \
+ include/././././Color.hpp include/./Functions.hpp include/./Gamepad.hpp \
+ include/./Image.hpp include/./Material.hpp include/./Matrix.hpp \
+ include/././raymath.hpp include/./Mesh.hpp include/././BoundingBox.hpp \
+ include/././Model.hpp include/./././Mesh.hpp include/./Model.hpp \
+ include/./ModelAnimation.hpp include/././Mesh.hpp include/./Mouse.hpp \
+ include/./Music.hpp include/./Ray.hpp include/././RayCollision.hpp \
+ include/./RaylibException.hpp include/./RayCollision.hpp \
+ include/./Rectangle.hpp include/./RenderTexture.hpp include/./Shader.hpp \
+ include/./Texture.hpp include/./Sound.hpp include/./Text.hpp \
+ include/./Texture.hpp include/./TextureUnmanaged.hpp include/./Touch.hpp \
+ include/./Vector2.hpp include/./Vector3.hpp include/./Vector4.hpp \
+ include/./VrStereoConfig.hpp include/./Wave.hpp include/./Window.hpp \
+ include/raymath.h include/raylib.h
+src/camera.hpp:
+src/player.hpp:
+include/raylib-cpp.hpp:
+include/./AudioDevice.hpp:
+include/././raylib.hpp:
+include/././raylib.h:
+include/././raylib-cpp-utils.hpp:
+include/././RaylibException.hpp:
+include/./././raylib.hpp:
+include/./AudioStream.hpp:
+include/./BoundingBox.hpp:
+include/./Camera2D.hpp:
+include/././Vector2.hpp:
+include/./././raymath.hpp:
+include/./././raymath.h:
+include/./././raylib-cpp-utils.hpp:
+include/./Camera3D.hpp:
+include/././Vector3.hpp:
+include/./Color.hpp:
+include/././Vector4.hpp:
+include/./Font.hpp:
+include/././TextureUnmanaged.hpp:
+include/./././Vector2.hpp:
+include/./././Material.hpp:
+include/././././raylib.hpp:
+include/././././raylib-cpp-utils.hpp:
+include/./././RaylibException.hpp:
+include/./././Image.hpp:
+include/././././RaylibException.hpp:
+include/././././Color.hpp:
+include/./Functions.hpp:
+include/./Gamepad.hpp:
+include/./Image.hpp:
+include/./Material.hpp:
+include/./Matrix.hpp:
+include/././raymath.hpp:
+include/./Mesh.hpp:
+include/././BoundingBox.hpp:
+include/././Model.hpp:
+include/./././Mesh.hpp:
+include/./Model.hpp:
+include/./ModelAnimation.hpp:
+include/././Mesh.hpp:
+include/./Mouse.hpp:
+include/./Music.hpp:
+include/./Ray.hpp:
+include/././RayCollision.hpp:
+include/./RaylibException.hpp:
+include/./RayCollision.hpp:
+include/./Rectangle.hpp:
+include/./RenderTexture.hpp:
+include/./Shader.hpp:
+include/./Texture.hpp:
+include/./Sound.hpp:
+include/./Text.hpp:
+include/./Texture.hpp:
+include/./TextureUnmanaged.hpp:
+include/./Touch.hpp:
+include/./Vector2.hpp:
+include/./Vector3.hpp:
+include/./Vector4.hpp:
+include/./VrStereoConfig.hpp:
+include/./Wave.hpp:
+include/./Window.hpp:
+include/raymath.h:
+include/raylib.h:
